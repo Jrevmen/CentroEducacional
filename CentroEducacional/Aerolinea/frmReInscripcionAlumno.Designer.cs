cee0c9151affeb0242a21278224c5b7e80225bf1
Eduardo <Eduardo>
2016-03-06 20:45:11 -0600

    Cambion en reinscripcion

---
+++ CentroEducacional/Aerolinea/frmReInscripcionAlumno.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Aerolinea
+{
+    partial class frmReInscripcionAlumno
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmReInscripcionAlumno));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.btnRefrescar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.txtBuscarPersona = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grdLLenarAlumno = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdLLenarAlumno)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btnBuscar);
+            this.panel1.Controls.Add(this.btnImprimir);
+            this.panel1.Controls.Add(this.btnRefrescar);
+            this.panel1.Controls.Add(this.btnCancelar);
+            this.panel1.Controls.Add(this.btnGuardar);
+            this.panel1.Controls.Add(this.btnEliminar);
+            this.panel1.Controls.Add(this.btnEditar);
+            this.panel1.Controls.Add(this.btnNuevo);
+            this.panel1.Location = new System.Drawing.Point(108, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(511, 55);
+            this.panel1.TabIndex = 8;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Image = ((System.Drawing.Image)(resources.GetObject("btnBuscar.Image")));
+            this.btnBuscar.Location = new System.Drawing.Point(393, 4);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(48, 42);
+            this.btnBuscar.TabIndex = 12;
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Image = ((System.Drawing.Image)(resources.GetObject("btnImprimir.Image")));
+            this.btnImprimir.Location = new System.Drawing.Point(339, 4);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(48, 42);
+            this.btnImprimir.TabIndex = 11;
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            // 
+            // btnRefrescar
+            // 
+            this.btnRefrescar.Image = ((System.Drawing.Image)(resources.GetObject("btnRefrescar.Image")));
+            this.btnRefrescar.Location = new System.Drawing.Point(285, 4);
+            this.btnRefrescar.Name = "btnRefrescar";
+            this.btnRefrescar.Size = new System.Drawing.Size(48, 42);
+            this.btnRefrescar.TabIndex = 10;
+            this.btnRefrescar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Image = ((System.Drawing.Image)(resources.GetObject("btnCancelar.Image")));
+            this.btnCancelar.Location = new System.Drawing.Point(231, 4);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(48, 42);
+            this.btnCancelar.TabIndex = 9;
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Image = ((System.Drawing.Image)(resources.GetObject("btnGuardar.Image")));
+            this.btnGuardar.Location = new System.Drawing.Point(177, 4);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(48, 42);
+            this.btnGuardar.TabIndex = 8;
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Enabled = false;
+            this.btnEliminar.Image = ((System.Drawing.Image)(resources.GetObject("btnEliminar.Image")));
+            this.btnEliminar.Location = new System.Drawing.Point(123, 4);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(48, 42);
+            this.btnEliminar.TabIndex = 7;
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Enabled = false;
+            this.btnEditar.Image = ((System.Drawing.Image)(resources.GetObject("btnEditar.Image")));
+            this.btnEditar.Location = new System.Drawing.Point(69, 4);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(48, 42);
+            this.btnEditar.TabIndex = 6;
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Image = ((System.Drawing.Image)(resources.GetObject("btnNuevo.Image")));
+            this.btnNuevo.Location = new System.Drawing.Point(15, 4);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(48, 42);
+            this.btnNuevo.TabIndex = 5;
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // txtBuscarPersona
+            // 
+            this.txtBuscarPersona.Location = new System.Drawing.Point(50, 20);
+            this.txtBuscarPersona.Name = "txtBuscarPersona";
+            this.txtBuscarPersona.Size = new System.Drawing.Size(277, 20);
+            this.txtBuscarPersona.TabIndex = 9;
+            this.txtBuscarPersona.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtBuscarPersona_KeyUp);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtBuscarPersona);
+            this.groupBox1.Location = new System.Drawing.Point(38, 73);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(351, 56);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Busqueda Alumno";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Carnet";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // grdLLenarAlumno
+            // 
+            this.grdLLenarAlumno.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdLLenarAlumno.Location = new System.Drawing.Point(8, 42);
+            this.grdLLenarAlumno.Name = "grdLLenarAlumno";
+            this.grdLLenarAlumno.Size = new System.Drawing.Size(572, 178);
+            this.grdLLenarAlumno.TabIndex = 13;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.grdLLenarAlumno);
+            this.groupBox2.Location = new System.Drawing.Point(39, 147);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(607, 226);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Informacion Estudiante";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(447, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(48, 42);
+            this.button1.TabIndex = 14;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // frmReInscripcionAlumno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 385);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmReInscripcionAlumno";
+            this.Text = "frmInscripcionAlumno";
+            this.Load += new System.EventHandler(this.frmInscripcionAlumno_Load);
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdLLenarAlumno)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Windows.Forms.Button btnRefrescar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.TextBox txtBuscarPersona;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView grdLLenarAlumno;
+        private System.Windows.Forms.GroupBox groupBox2;
+        public System.Windows.Forms.Button btnEliminar;
+        public System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
---
+++ CentroEducacional/Aerolinea/frmRePrincipalInscripcion.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Navegador;
+using ConexionODBC;
+
+using System.Data.Odbc;
+using System.Data.SqlClient;
+namespace Aerolinea
+{
+    public partial class frmRePrincipalInscripcion : Form
+    {
+        
+
+        public static OdbcCommand _comando;
+        public static OdbcDataReader _reader;
+        //se valida el actvo
+        public static string ACTIVO;
+        public frmRePrincipalInscripcion()
+        {
+            InitializeComponent();
+            funActualizarGrid();
+        }
+        private void funActualizarGrid()
+        {
+            clasnegocio cnegocio = new clasnegocio();
+            cnegocio.funconsultarRegistros("carnet", "SELECT encabezado_incripcion.codigoInscripcion as NoIsncripcion,encabezado_incripcion.codigoCarnet as Carnet, encabezado_incripcion.estado as Estado,persona.nombre as Nombre, persona.apellido as Apellido, carrera.nombre as Carrera,jornada.nombre as Jornada from encabezado_incripcion,persona,carnet,carrera,jornada WHERE encabezado_incripcion.codigoCarnet=carnet.codigoCarnet and carnet.codigopersona=persona.codigopersona and carnet.codigoCarrera=carrera.codigoCarrera and carnet.codigoJornada=jornada.codigoJornada", "consulta", grdInscripcion);
+//textBox1.Text = grdInscripcion.Rows[codigoInscripcion.RowIndex].Cells[0].Value.ToString();
+           }
+        
+
+       // private void funionactivo() {
+         //   string comand;
+           // clasnegocio cnegocio1 = new clasnegocio();
+
+            //cnegocio1.funconsultarRegistros("carnet", "SELECT encabezado_incripcion.codigoInscripcion as NoIsncripcion,encabezado_incripcion.codigoCarnet as Carnet, encabezado_incripcion.estado as Estado,persona.nombre as Nombre, persona.apellido as Apellido, carrera.nombre as Carrera,jornada.nombre as Jornada from encabezado_incripcion,persona,carnet,carrera,jornada WHERE encabezado_incripcion.codigoCarnet=carnet.codigoCarnet and carnet.codigopersona=persona.codigopersona and carnet.codigoCarrera=carrera.codigoCarrera and carnet.codigoJornada=jornada.codigoJornada", "consulta", grdInscripcion);
+            //SqlCommand cmd = new SqlCommand(comand, cnegocio1);
+       // }
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            frmReInscripcionAlumno temp = new frmReInscripcionAlumno();
+            temp.Show();
+        }
+
+        private void btnRefrescar_Click(object sender, EventArgs e)
+        {
+            funActualizarGrid();
+        }
+
+        private void btnIrPrimero_Click(object sender, EventArgs e)
+        {
+            clasnegocio cnegocio = new clasnegocio();
+            cnegocio.funPrimero(grdInscripcion);
+        }
+
+        private void btnAnterior_Click(object sender, EventArgs e)
+        {
+            clasnegocio cnegocio = new clasnegocio();
+            cnegocio.funAnterior(grdInscripcion);
+        }
+
+        private void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            clasnegocio cnegocio = new clasnegocio();
+            cnegocio.funSiguiente(grdInscripcion);
+        }
+
+        private void btnIrUltimo_Click(object sender, EventArgs e)
+        {
+            clasnegocio cnegocio = new clasnegocio();
+            cnegocio.funUltimo(grdInscripcion);
+        }
+
+        private void txtBuscar_KeyUp(object sender, KeyEventArgs e)
+        {
+            clasnegocio cnegocio = new clasnegocio();
+            cnegocio.funconsultarRegistros("carnet", "SELECT encabezado_incripcion.codigoInscripcion as NoIsncripcion,encabezado_incripcion.codigoCarnet as Carnet, encabezado_incripcion.estado as Estado,persona.nombre as Nombre, persona.apellido as Apellido, carrera.nombre as Carrera,jornada.nombre as Jornada from encabezado_incripcion,persona,carnet,carrera,jornada WHERE encabezado_incripcion.codigoCarnet=carnet.codigoCarnet and carnet.codigopersona=persona.codigopersona and carnet.codigoCarrera=carrera.codigoCarrera and carnet.codigoJornada=jornada.codigoJornada and encabezado_incripcion.codigoCarnet='" + txtBuscar.Text + "'", "consulta", grdInscripcion);
+        }
+
+        private void grdInscripcion_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            //se extraen datos para comparacion
+
+            claseUsuario.funobtenerBitacora(claseUsuario.varibaleUsuario, "Consulta", "Alumno");
+            textBox1.Text = grdInscripcion.CurrentRow.Cells["Estado"].Value.ToString();
+
+            string sCodInscripcion = grdInscripcion.Rows[grdInscripcion.CurrentCell.RowIndex].Cells[0].Value.ToString();
+            string sCarnet = grdInscripcion.Rows[grdInscripcion.CurrentCell.RowIndex].Cells[1].Value.ToString();
+            frmReInscripcionAlumno temp = new frmReInscripcionAlumno(sCodInscripcion, sCarnet);
+            //condicional para habilitar botones para rescripcion
+            if (textBox1.Text == textBox2.Text)
+            {
+
+                temp.btnEditar.Enabled = true;
+                temp.btnEliminar.Enabled = false;
+                temp.Show();
+            }
+            else {
+
+                temp.btnEditar.Enabled = false;
+                temp.btnEliminar.Enabled = true;
+                temp.Show();
+            }
+
+              
+               
+            
+            
+        }
+
+        private void grdInscripcion_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void grupoFiltrar_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmRePrincipalInscripcion_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+}
